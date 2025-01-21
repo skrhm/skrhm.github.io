b4719a998ebd3b12aa4711d95c419b4659b4a26b
skrhm <kuzira.ch@gmail.com>
2025-01-21 17:16:56 +0900

    IPアドレス表示のIDを変更し、スタイルを統一

--- scripts/ip.js
+++ scripts/ip.js
@@ -1,6 +1,6 @@
 fetch('https://api.ipify.org?format=json')
     .then(response => response.json())
     .then(data => {
-        document.getElementById('ip').textContent = data.ip;
+        document.getElementById('ipaddress').textContent = data.ip;
     })
     .catch(error => console.error('Error:', error));
